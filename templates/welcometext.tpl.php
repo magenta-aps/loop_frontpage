d537971a5189d16bd1734821cb505cc828bd6958
Lars Peter Thomsen <larsp@magenta.dk>
2015-08-18 11:01:04 +0200

    Frontpage welcome text

---
+++ templates/welcometext.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Displays the frontpage welcome text
+ *
+ * Available variables:
+ * - $text: The complete text ready for print.
+ */
+?>
+<div class="welcometext"><?php print $text; ?></div>
